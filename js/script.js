2250ad95aa1fb61b56fde299d28ed01cfdd25724
unknown <ndbendale2004@gmail.com>
2021-05-22 15:22:30 +0530

    added the weeks in a seperate html file

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ timeline
     .to('.garbage', 3 , {y: -400})
     .to('.rock', 3 , {y: -100}, '-=3')
     .to('.logo', 3 , {y: -300}, '-=3')
-    // .fromTo('.img',{y: -50}, {y: 0, duration: 3}, '-=3')
+    .fromTo('.img',{y: -50}, {y: 0, duration: 3}, '-=3')
     .to('.notes', 3 , {top: '0%'}, '-=3');
 
 
